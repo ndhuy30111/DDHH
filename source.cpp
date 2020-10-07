7bb1c9f5a19d885ec47ee327fd5a744945bea1ff
ttdat4 <dh51702456@student.stu.edu.vn>
2020-10-07 11:31:26 +0700

    cong git commit -m cong   git config --global user.name ttdat4 .. qq exit quit q: :q

--- source.cpp
+++ source.cpp
@@ -1,6 +1,6 @@
 int USCLN(int a, int b)
 {
-
+	
 }
 void hello()
 {
@@ -12,7 +12,12 @@ int tru(int a, int b)
 	cout<<a-b;
 int cong(int a, int b)
 {
-
+	cout <<"\nNhap so nguyen a:";
+	cin>> a;
+	cout <<"\nNhap so nguyen b:";
+	cin >>b;
+	cout <<"\nTong hai so a va b la:" << a + b;
+	return a+b;
 }
 int nhan(int a, int b)
 {
