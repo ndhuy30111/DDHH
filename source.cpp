3422d562d570c39e02c4aefcbbf04696a8df97f2
anhaipy <quynhai113@gmail.com>
2020-10-07 11:41:04 +0700

    Hai 1

--- source.cpp
+++ source.cpp
@@ -17,3 +17,14 @@ int cong(int a, int b)
 int nhan(int a, int b)
 {
 }
+//Hairef
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+
+	cin.get();
+	return 0;
+}
