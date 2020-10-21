b83072d6495c41aa1e533a36a84f552396de0766
Chau Hai <chauhai13599@gmail.com>
2020-10-21 10:55:34 +0700

    Hieu2So

--- BT_GIT/BT_GIT/Source.cpp
+++ BT_GIT/BT_GIT/Source.cpp
@@ -1,12 +1,22 @@
+#include <cstdlib>
 #include <iostream>
 using namespace std;
-int main() {
+//void nhap() {
+//	int a, b;
+//	cout << a + b << endl;
+//	cout << a - b << endl;
+//	cout << a * b << endl;
+//cout << a / b << endl;}
+int main()
+{
+	int newnum1, newnum2, Hieu;
+	newnum1 = rand() % 100 + 1;
+	newnum2 = rand() % 100 + 1;
+	Hieu = newnum1 - newnum2;
+	cout << "So ngau nhien thu nhat la: " << newnum1 << endl;
+	cout << "So ngau nhien thu 2 la: " << newnum2 << endl;
+	cout << "Hieu 2 so la: " << Hieu << endl;
+	return Hieu;
+	system("pause");
 
-}
-void nhap() {
-	int a, b;
-	cout << a + b << endl;
-	cout << a - b << endl;
-	cout << a * b << endl;
-	cout << a / b << endl;
 }
