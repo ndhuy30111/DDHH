29a0db3b03ccc61594d89709861ddcc7a357cb85
ndhuy30111 <dh51701452@student.stu.edu.vn>
2020-10-21 10:36:03 +0700

    phep toan

---
+++ BT_GIT/BT_GIT/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+}
+void nhap() {
+	int a, b;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+}
