ae3cc12cfc5baa7cc628440fd8184a298541011f
ndhuy30111 <dh51701452@student.stu.edu.vn>
2020-10-21 11:40:06 +0700

    thanhdat

--- BT_GIT/BT_GIT/Source.cpp
+++ BT_GIT/BT_GIT/Source.cpp
@@ -3,13 +3,29 @@
 #include <time.h>
 #include <cstdlib>
 using namespace std;
-int main() {
 
-}
 void nhap() {
 	int a, b;
+	cout << "Nhap a";
+	cout << "Nhap b";
+	cin >> a;
+	cin >> b;
 	cout << a + b << endl;
 	cout << a - b << endl;
 	cout << a * b << endl;
 	cout << a / b << endl;
+}
+void RandomTong()
+{
+	srand((int)time(0));
+	int a = rand() % 101;
+	int b = rand() % 101;
+	cout << a << endl;
+	cout << b << endl;
+	cout << "Tong la: " << a + b << endl;
+}
+int main() {
+	RandomTong();
+	system("pause");
+	return 0;
 }
